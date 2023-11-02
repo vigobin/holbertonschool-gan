54fa0da13ce8ddf2f189ba5002cb0752ce7d2a20
vigobin <vgobin@outlook.com>
2023-11-01 23:32:04 -0500

    Precision to float16

---
+++ dcgan/models/PrecisionTuning.py
@@ -0,0 +1,184 @@
+# -*- coding: utf-8 -*-
+"""Baseline.ipynb
+"""
+
+import matplotlib.pyplot as plt
+import os
+import time
+
+import tensorflow as tf
+from tensorflow.keras import layers
+
+import wandb
+wandb.login()
+wandb.init(project='DCGAN', name='Experiment1')
+
+config = wandb.config
+config.BUFFER_SIZE = 60000
+config.BATCH_SIZE = 128
+config.EPOCHS = 20
+config.noise_dim = 100
+config.num_to_generate = 16
+config.precision = 'float16'
+
+wandb.config.update(config)
+
+(train_images, train_labels), (_, _) = tf.keras.datasets.mnist.load_data()
+
+train_images = train_images.reshape(
+   train_images.shape[0], 28, 28, 1).astype(config.precision)
+train_images = (train_images - 127.5) / 127.5
+
+# BUFFER_SIZE = 60000
+# BATCH_SIZE = 256
+
+train_dataset = tf.data.Dataset.from_tensor_slices(
+   train_images).shuffle(config.BUFFER_SIZE).batch(config.BATCH_SIZE)
+
+
+def make_generator_model():
+    model = tf.keras.Sequential()
+    model.add(layers.Dense(7*7*256, use_bias=False, input_shape=(100,)))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Reshape((7, 7, 256)))
+    assert model.output_shape == (None, 7, 7, 256)  # None is the batch size
+
+    model.add(layers.Conv2DTranspose(
+       128, (5, 5), strides=(1, 1), padding='same', use_bias=False))
+    assert model.output_shape == (None, 7, 7, 128)
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Conv2DTranspose(
+       64, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+    assert model.output_shape == (None, 14, 14, 64)
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(
+       layers.Conv2DTranspose(1, (
+          5, 5), strides=(2, 2), padding='same', use_bias=False,
+          activation='tanh'))
+    assert model.output_shape == (None, 28, 28, 1)
+
+    return model
+
+
+generator = make_generator_model()
+
+noise = tf.random.normal([1, 100])
+generated_image = generator(noise, training=False)
+
+plt.imshow(generated_image[0, :, :, 0], cmap='gray')
+
+
+def make_discriminator_model():
+    model = tf.keras.Sequential()
+    model.add(layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same',
+                            input_shape=[28, 28, 1]))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.3))
+
+    model.add(layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same'))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.3))
+
+    model.add(layers.Flatten())
+    model.add(layers.Dense(1))
+
+    return model
+
+
+discriminator = make_discriminator_model()
+decision = discriminator(generated_image)
+print(decision)
+
+cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+
+
+def discriminator_loss(real_output, fake_output):
+    real_loss = cross_entropy(tf.ones_like(real_output), real_output)
+    fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
+    total_loss = real_loss + fake_loss
+    return total_loss
+
+
+def generator_loss(fake_output):
+    return cross_entropy(tf.ones_like(fake_output), fake_output)
+
+
+generator_optimizer = tf.keras.optimizers.Adam(1e-4)
+discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
+
+# EPOCHS = 1
+# noise_dim = 100
+# num_to_generate = 16
+
+seed = tf.random.normal([config.num_to_generate, config.noise_dim])
+
+
+@tf.function
+def train_step(images):
+    noise = tf.random.normal([config.BATCH_SIZE, config.noise_dim])
+
+    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+        generated_images = generator(noise, training=True)
+
+        real_output = discriminator(images, training=True)
+        fake_output = discriminator(generated_images, training=True)
+
+        gen_loss = generator_loss(fake_output)
+        disc_loss = discriminator_loss(real_output, fake_output)
+
+    gradients_of_generator = gen_tape.gradient(
+       gen_loss, generator.trainable_variables)
+    gradients_of_discriminator = disc_tape.gradient(
+       disc_loss, discriminator.trainable_variables)
+
+    generator_optimizer.apply_gradients(
+       zip(gradients_of_generator, generator.trainable_variables))
+    discriminator_optimizer.apply_gradients(
+       zip(gradients_of_discriminator, discriminator.trainable_variables))
+
+    return gen_loss, disc_loss
+
+
+def train(dataset, epochs):
+    for epoch in range(epochs):
+        start = time.time()
+
+    for image_batch in dataset:
+        gen_loss, disc_loss = train_step(image_batch)
+        wandb.log(
+            {'Discriminator Loss': disc_loss, 'Generator Loss': gen_loss})
+
+    generate_and_save_images(generator,
+                             epoch + 1,
+                             seed)
+
+    print('Time for epoch {} is {} sec'.format(epoch + 1, time.time()-start))
+
+    generate_and_save_images(generator, epochs, seed)
+
+
+def generate_and_save_images(model, epoch, test_input):
+    predictions = model(test_input, training=False)
+
+    fig = plt.figure(figsize=(4, 4))
+
+    for i in range(predictions.shape[0]):
+        plt.subplot(4, 4, i+1)
+        plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
+        plt.axis('off')
+
+    img = 'image_at_epoch_{:04d}.png'.format(epoch)
+    plt.savefig(img)
+    plt.show()
+
+    wandb.log({"Images": [wandb.Image(img, caption=f'Epoch {epoch}')]})
+
+
+train(train_dataset, config.EPOCHS)
+wandb.finish()
